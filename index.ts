c326c47c7d2f907ba22482f404376e8938675a17
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-11-06 12:06:11 +0000

    Add documentation for each type.

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
+/**
+ * An object containing elements of type T, keyed by strings.
+ */
 export interface Dictionary<T> {
     [key: string]: T;
 }
 
+/**
+ * A read-only object containing elements of type T, keyed by strings.
+ */
 export interface ReadonlyDictionary<T> {
     readonly [key: string]: T;
 }
 
+/**
+ * An object containing elements of type T, keyed by numbers.
+ */
 export interface NumberMap<T> {
     [key: number]: T;
 }
 
+/**
+ * A read-only object containing elements of type T, keyed by numbers.
+ */
 export interface ReadonlyNumberMap<T> {
     readonly [key: number]: T;
 }

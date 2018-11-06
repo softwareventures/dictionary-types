5359666d42b8a3052e477ddeaf73e5cf3e763e62
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-11-06 12:45:00 +0000

    Add usage examples for Dictionary and ReadonlyDictionary.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
 /**
  * An object containing elements of type T, keyed by strings.
+ *
+ * ```
+ * const scores: Dictionary<number> = {
+ *     "Amelia": 4,
+ *     "Riley": 7,
+ *     "April": 5
+ * };
+ *
+ *  scores["Xander"] = 3;
+ * ```
  */
 export interface Dictionary<T> {
     [key: string]: T;
@@ -7,6 +17,22 @@ export interface Dictionary<T> {
 
 /**
  * A read-only object containing elements of type T, keyed by strings.
+ *
+ * ```typescript
+ * function winner(scores: ReadonlyDictionary<number>): string {
+ *     let winner = "";
+ *     let highScore = 0;
+ *
+ *     for (const name of Object.keys(scores)) {
+ *         if (scores[name] > highScore) {
+ *             highScore = scores[name];
+ *             winner = name;
+ *         }
+ *     }
+ *
+ *     return name;
+ * }
+ * ```
  */
 export interface ReadonlyDictionary<T> {
     readonly [key: string]: T;
